ce5bbe93ed77646ab81dd71bf8354dd155a9d3bd
DoubleF3lix <48461905+Double-Felix@users.noreply.github.com>
2021-02-02 19:37:29 -0600

    Added missing selector registries

--- onyx/__init__.py
+++ onyx/__init__.py
@@ -2,3 +2,4 @@ from onyx.pack_manager import *
 from onyx.commands import Commands as commands
 from onyx.selector import Selector as selector
 from onyx.util import *
+from onyx.registries import *
--- onyx/registries.py
+++ onyx/registries.py
@@ -1,7 +1,23 @@
 import enum
 
-class difficulty(enum.Enum):
-    adventure = "adventure"
+
+class gamemode(enum.Enum):
+    survival = "survival"
     creative = "creative"
     spectator = "spectator"
-    survival = "survival"
+    adventure = "adventure"
+
+
+class selector_type(enum.Enum):
+    all_players = "@a"
+    all_entities = "@e"
+    random_player = "@r"
+    nearest_player = "@p"
+    selected_entity = "@s"
+
+
+class entity_sort(enum.Enum):
+    arbitrary = "arbitrary"
+    furthest = "furthest"
+    nearest = "nearest"
+    random = "random"
