f5afc1a3960f9367584bdac532624fbe6b0ac4f6
DoubleF3lix <48461905+Double-Felix@users.noreply.github.com>
2021-02-02 19:36:21 -0600

    Added functions to convert dictionaries to advancement and score selector parameters

--- onyx/dev_util.py
+++ onyx/dev_util.py
@@ -1,5 +1,5 @@
 import re
-import onyx.selector
+import json
 
 def convert_mcfunction_path(mcfunction_path):
     path = mcfunction_path.split(":")
@@ -21,6 +21,37 @@ def snakify(text):
 
     return '_'.join(output)
 
+def dict_to_advancement_selector(arg):
+    # {"thing/1": {"thing/12": True}, "thing/2": False}
+    tmp = []
+    for key, item in arg.items():
+        if isinstance(item, bool):
+            tmp.append(f"{key}={json.dumps(item)}")   
+        # Assume type is dictionary
+        else:
+            # Grab only the first key and its value (stored in a tuple, key is 0, value is 1)
+            first_dict_pair = list(item.items())[0]
+            tmp.append(f"{key}={{{first_dict_pair[0]}={json.dumps(first_dict_pair[1])}}}")
+
+    return f"{{{', '.join(tmp)}}}"
+
+def dict_to_score_selector(arg):
+    # {scoreboardObj1: 3, scoreboardObj2: 4}
+    tmp = []
+    for key, item in arg.items():
+        tmp.append(f"{key}={translate(item)}")   
+
+    return f"{{{', '.join(tmp)}}}"
+
 def translate(obj):
-    if isinstance(obj, onyx.selector.Selector):
+    from onyx.class_types import Buildable
+    import enum
+
+    if isinstance(obj, Buildable):
         return obj.build()
+    elif isinstance(obj, enum.Enum):
+        return obj.value
+    elif obj is None:
+        return ""
+    else:
+        return obj
